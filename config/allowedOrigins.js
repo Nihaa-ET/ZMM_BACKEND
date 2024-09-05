3c1000362ab517761efc2d3cdce1d1cc8c5bb787
Nihaa-ET <Nihaa@Etpl2024>
2024-09-05 14:40:44 +0530

    first commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins =[
+    'https://zeromiddleman.onrender.com',
+    'http://localhost:3000 ',
+    'https://directories.zeromiddleman.com',
+  ]
+
+  module.exports = allowedOrigins
